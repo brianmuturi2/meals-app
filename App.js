480cf78bb79916e9b13fcab352f012fab5997b5c
brianmuturi2 <brianmuturi2@gmail.com>
2022-09-23 17:43:18 +0300

    feat(meals app): added drawer navigation

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailsScreen from './screens/MealDetailsScreen';
+import CategoriesScreen from './screens/CategoriesScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import {Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+    return (
+        <Drawer.Navigator screenOptions={{
+            headerStyle: {backgroundColor: '#351401'},
+            headerTintColor: 'white',
+            sceneContainerStyle: {backgroundColor: '#3f2f25'},
+            drawerContentStyle: {backgroundColor: '#351401'},
+            drawerInactiveTintColor: 'white',
+            drawerActiveTintColor: '#351401',
+            drawerActiveBackgroundColor: '#e4baa1'
+        }}>
+            <Drawer.Screen
+                name={'Meals Categories'}
+                component={CategoriesScreen}
+                options={{
+                    title: 'Meals Categories',
+                    drawerIcon: ({color, size}) => (<Ionicons name={'list'} color={color} size={size}/>)
+                }}
+            />
+            <Drawer.Screen
+                name={'Favorites'}
+                component={FavoritesScreen}
+                options={{
+                    title: 'Favorites',
+                    drawerIcon: ({color, size}) => (<Ionicons name={'star'} color={color} size={size}/>)
+                }}
+            />
+        </Drawer.Navigator>
+    );
+}
 
 export default function App() {
   return (
@@ -19,10 +54,11 @@ export default function App() {
                 contentStyle: {backgroundColor: '#3f2f25'}
             }}>
                 <Stack.Screen
-                    name="Meals Categories"
-                    component={CategoriesScreen}
+                    name="Drawer"
+                    component={DrawerNavigator}
                     options={{
                         title: 'All Categories',
+                        headerShown: false
                     }}
                 />
                 <Stack.Screen
@@ -35,7 +71,9 @@ export default function App() {
                         }
                     }}*/
                 />
-                <Stack.Screen name="Meals Details" component={MealDetailsScreen}/>
+                <Stack.Screen
+                    name="Meals Details"
+                    component={MealDetailsScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
       </>
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,13 +1,27 @@
-import {Text, View, ScrollView, Image, StyleSheet} from 'react-native';
+import {useLayoutEffect} from 'react';
+import {Text, View, ScrollView, Image, StyleSheet, Button} from 'react-native';
 import {MEALS} from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetails/Subtitle';
 import List from '../components/MealDetails/List';
+import IconButton from '../components/IconButton';
 
-function MealDetailsScreen({route}) {
+function MealDetailsScreen({route, navigation}) {
 
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log('Pressed!');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <IconButton onPress={headerButtonPressHandler} icon={'star'} color={'white'} />
+            }
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <ScrollView style={styles.container}>
